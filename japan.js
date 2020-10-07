333cc847b15ee6f0d9a748915ab2b7c2801f2e7b
Arkadia977 <arkadia@argalie.fr>
2020-10-07 23:27:20 +0200

    Big Change JS

--- japan.js
+++ japan.js
@@ -1,60 +1,127 @@
-function randList(list){
-    return Math.floor(Math.random() * list.length);
-}
-
-function randKana(){
-    return kanalist[randList(kanalist)];
-}
-
-function tableGen(){
-    $("#tbodykanaTable").empty();
-    let result = kanaResult["hiragana"];
-    for (let i = 0; i < Object.keys(kanaResult["hiragana"]).length; i++) {
-        if(result[kanalistAll[i]]["success"]!=0 || result[kanalistAll[i]]["error"]!=0){
-            $("#tbodykanaTable").append("<tr><td>"+result[kanalistAll[i]]["name"]+"</td><td>"+result[kanalistAll[i]]["success"]+"</td><td>"+result[kanalistAll[i]]["error"]+"</td></tr>");
+function getKana(list = ["B","K","S","T","N","H","M","Y","R","W"]){
+    listKana = [];
+    for (const [key, value] of Object.entries(kana)){
+        if(list.includes(kana[key]["group"])){
+            listKana.push( key );
         }
     }
+    return listKana;
 }
 
-let kanagroup = [];
-kanagroup[0] = ["A","I","U","E","O"];
-kanagroup[1] = ["KA","KI","KU","KE","KO"];
-kanagroup[2] = ["SA","SHI","SU","SE","SO"];
-kanagroup[3] = ["TA","CHI","TSU","TE","TO"];
-kanagroup[4] = ["NA","NI","NU","NE","NO"];
-kanagroup[5] = ["HA","HI","FU","HE","HO"];
-kanagroup[6] = ["MA","MI","MU","ME","MO"];
-kanagroup[7] = ["YA","YU","YO"];
-kanagroup[8] = ["RA","RI","RU","RE","RO"];
-kanagroup[9] = ["WA","WO"];
-kanagroup[10] = ["N"];
+function randKana(list = ["B","K","S","T","N","H","M","Y","R","W"]){
+    listKana = getKana(list);
+    return listKana[Math.floor(Math.random() * listKana.length)];
+}
 
-let kanalist = [];
-let kanalistAll = [];
-for (let index = 0; index < kanagroup.length; index++) {
-    for (let i = 0; i < kanagroup[index].length; i++) {
-        kanalistAll.push(kanagroup[index][i]);
+function getKanaRatio(kanaGet,kanaSelect){
+    total = kana[kanaGet][kanaSelect]["success"]+kana[kanaGet][kanaSelect]["error"];
+    ratio = (kana[kanaGet][kanaSelect]["success"]/total)*100;
+    ratio = ratio || 0
+    return ratio;
+}
+
+function getKanaScore(kanaGet,kanaSelect){
+    return kana[kanaGet][kanaSelect]["score"];
+}
+
+function addKanaSuccess(kanaGet,kanaSelect){
+    kana[kanaGet][kanaSelect]["success"]++;
+    score = kana[kanaGet][kanaSelect]["score"];
+    if(score<100){
+        kana[kanaGet][kanaSelect]["score"]++;
     }
 }
 
-function genKanaList(){
-    kanalist = [];
-    for (let index = 0; index < kanagroup.length; index++) {
-        for (let i = 0; i < kanagroup[index].length; i++) {
-            if($('input[name='+index+']').is(':checked')){
-                kanalist.push(kanagroup[index][i]);
-            }
-        }
+function addKanaError(kanaGet,kanaSelect){
+    kana[kanaGet][kanaSelect]["error"]++;
+    score = kana[kanaGet][kanaSelect]["score"];
+    if(score>0){
+        kana[kanaGet][kanaSelect]["score"]--;
     }
 }
 
-function genResult(){
-    kanaResult = [];
-    kanaResult["hiragana"] = [];
-    for (let index = 0; index < kanalistAll.length; index++) {
-        kanaResult["hiragana"][kanalistAll[index]] = [];
-        kanaResult["hiragana"][kanalistAll[index]]["name"] = kanalistAll[index];
-        kanaResult["hiragana"][kanalistAll[index]]["success"] = 0;
-        kanaResult["hiragana"][kanalistAll[index]]["error"] = 0;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+// for (const [key, value] of Object.entries(kanagroup)) {
+//     json[key] = [];
+//     json[key][value[index] = [];
+//     json[key][value[index]]["hiragana"] = [];
+//     json[key][value[index]]["hiragana"] = [];
+//     for (let index = 0; index < value.length; index++) {
+//         json[key][value[index]]["hiragana"]["success"] = 0;
+//         json[key][value[index]]["hiragana"]["error"] = 0;
+//         json[key][value[index]]["katakana"]["success"] = 0;
+//         json[key][value[index]]["katakana"]["error"] = 0;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+// function randList(list){
+//     return Math.floor(Math.random() * list.length);
+// }
+
+// function randKana(){
+//     return kanalist[randList(kanalist)];
+// }
+
+// function tableGen(){
+//     $("#tbodykanaTable").empty();
+//     let result = kanaResult["hiragana"];
+//     for (let i = 0; i < Object.keys(kanaResult["hiragana"]).length; i++) {
+//         if(result[kanalistAll[i]]["success"]!=0 || result[kanalistAll[i]]["error"]!=0){
+//             $("#tbodykanaTable").append("<tr><td>"+result[kanalistAll[i]]["name"]+"</td><td>"+result[kanalistAll[i]]["success"]+"</td><td>"+result[kanalistAll[i]]["error"]+"</td></tr>");
+//         }
+//     }
+// }
+
+
+
+// console.log(JSON.stringify(json));
+
+
+// let kanalist = [];
+// let kanalistAll = [];
+// for (let index = 0; index < kanagroup.length; index++) {
+//     for (let i = 0; i < kanagroup[index].length; i++) {
+//         kanalistAll.push(kanagroup[index][i]);
+//     }
+// }
+
+// function genKanaList(){
+//     kanalist = [];
+//     for (let index = 0; index < kanagroup.length; index++) {
+//         for (let i = 0; i < kanagroup[index].length; i++) {
+//             if($('input[name='+index+']').is(':checked')){
+//                 kanalist.push(kanagroup[index][i]);
+//             }
+//         }
+//     }
+// }
+
+// function genResult(){
+//     kanaResult = [];
+//     kanaResult["hiragana"] = [];
+//     for (let index = 0; index < kanalistAll.length; index++) {
+//         kanaResult["hiragana"][kanalistAll[index]] = [];
+//         kanaResult["hiragana"][kanalistAll[index]]["name"] = kanalistAll[index];
+//         kanaResult["hiragana"][kanalistAll[index]]["success"] = 0;
+//         kanaResult["hiragana"][kanalistAll[index]]["error"] = 0;
+//     }
+// }
---
+++ kana.js
@@ -0,0 +1,600 @@
+const kana = {
+    "A": {
+        "group": "B",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "I": {
+        "group": "B",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "U": {
+        "group": "B",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "E": {
+        "group": "B",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "O": {
+        "group": "B",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "KA": {
+        "group": "K",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "KI": {
+        "group": "K",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "KU": {
+        "group": "K",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "KE": {
+        "group": "K",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "KO": {
+        "group": "K",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "SA": {
+        "group": "S",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "SHI": {
+        "group": "S",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "SU": {
+        "group": "S",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "SE": {
+        "group": "S",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "SO": {
+        "group": "S",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "TA": {
+        "group": "T",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "CHI": {
+        "group": "T",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "TSU": {
+        "group": "T",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "TE": {
+        "group": "T",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "TO": {
+        "group": "T",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "N": {
+        "group": "N",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "NA": {
+        "group": "N",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "NI": {
+        "group": "N",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "NU": {
+        "group": "N",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "NE": {
+        "group": "N",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "NO": {
+        "group": "N",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "HA": {
+        "group": "H",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "HI": {
+        "group": "H",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "FU": {
+        "group": "H",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "HE": {
+        "group": "H",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "HO": {
+        "group": "H",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "MA": {
+        "group": "M",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "MI": {
+        "group": "M",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "MU": {
+        "group": "M",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "ME": {
+        "group": "M",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "MO": {
+        "group": "M",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "YA": {
+        "group": "Y",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "YU": {
+        "group": "Y",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "YO": {
+        "group": "Y",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "RA": {
+        "group": "R",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "RI": {
+        "group": "R",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "RU": {
+        "group": "R",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "RE": {
+        "group": "R",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "RO": {
+        "group": "R",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "WA": {
+        "group": "W",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    },
+    "WO": {
+        "group": "W",
+        "hiragana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        },
+        "katakana": {
+            "success": 0,
+            "error": 0,
+            "score": 0
+        }
+    }
+}
