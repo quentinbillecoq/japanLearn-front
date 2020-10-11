50a1464cef37d3c6e121e187412b9bada8bf49b4
Arkadia977 <arkadia@argalie.fr>
2020-10-11 23:06:47 +0200

    Update GUI

--- japan.js
+++ japan.js
@@ -41,6 +41,18 @@ function addKanaError(kanaGet,kanaSelect){
 }
 
 
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+
 
 
 
